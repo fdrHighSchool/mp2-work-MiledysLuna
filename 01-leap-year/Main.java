20f3f0e3664f074be1720b81b6050d63f9b9a5ba
MiledysLuna <114938949+MiledysLuna@users.noreply.github.com>
2022-12-02 12:05:39 -0500

    Create Main.java

---
+++ 01-leap-year/Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+  system.out.println(isLeapYear(2022))
+  system.out.println(isLeapYear(2024))
+  system.out.println(isLeapYear(1900))
+  system.out.println(isLeapYear(2000))
+    public static boolean(isLeapYear(int year)) {
+    boolean status 
+    }
+  }
+}c
